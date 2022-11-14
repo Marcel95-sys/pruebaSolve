61eb391ba56a293b6e1d49457465f92b09e3b29f
Marcel95-sys <farremarcel95@gmail.com>
2022-11-14 22:55:39 +0100

    pruebas postman para comunicacion con db

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // Importing module
 const express_1 = __importDefault(require("express"));
+const sqlite3_1 = require("sqlite3");
 const app = (0, express_1.default)();
 const PORT = 3000;
-app.post('/', (req, res) => {
-});
+const db = new sqlite3_1.Database('base_datos.db');
 // Handling GET / Request
-app.get('/', (req, res) => {
-    res.send('Welcome to typescript backend!');
+app.get('', (_, res) => {
+    res.send(db.get('SELECT ALL() % 100 as result'));
+});
+app.post('/:nick/:email/:passw/:nump', (req, res) => {
+    db.exec('SELECT nick FROM users INSERT ' + req.params.nick);
+    db.exec('SELECT email FROM users INSERT ' + req.params.email);
+    db.exec('SELECT password FROM users INSERT ' + req.params.passw);
+    db.exec('SELECT numphone FROM users INSERT ' + req.params.nump);
+    res.send(db.get("SELECT FROM users  WHERE email = " + req.params.email));
+});
+app.delete('/:id', (req, res) => {
+    db.exec('DELETE FROM users WHERE ' + req.params.id + '=?');
+    return;
+});
+app.put('/', (req, res) => {
+    //db.exec('UPDATE users SET ')
 });
 // Server setup
 app.listen(PORT, () => {
--- server.ts
+++ server.ts
@@ -5,23 +5,25 @@ import { Database } from 'sqlite3';
 const app = express();
 const PORT:Number=3000;
 
-const db = new Database('db.dataBase');
+const db = new Database('base_datos.db');
 
 
 // Handling GET / Request
-app.get('/', (req, res) => {
-	db.get('SELECT ALL() % 100 as result', (_, res) => console.log(res));	
+app.get('', (_,res) => {
+	res.send(db.get('SELECT ALL() % 100 as result'));	
 })
 
-app.post('/:nick/:email/:passw/:nump', (req, _) => {
+app.post('/:nick/:email/:passw/:nump', (req, res) => {
 	db.exec('SELECT nick FROM users INSERT ' + req.params.nick);	
 	db.exec('SELECT email FROM users INSERT ' + req.params.email);
 	db.exec('SELECT password FROM users INSERT ' + req.params.passw);
-	db.exec('SELECT numphone FROM users INSERT ' + req.params.nump)
+	db.exec('SELECT numphone FROM users INSERT ' + req.params.nump);
+	res.send(db.get("SELECT FROM users  WHERE email = " + req.params.email ));
 })
 
 app.delete('/:id', (req, res) => {
-	db.exec('DELETE FROM users WHERE ' + req.params.id + '=?')
+	db.exec('DELETE FROM users WHERE ' + req.params.id + '=?');
+	return;
 })
 
 app.put('/', (req, res) => {

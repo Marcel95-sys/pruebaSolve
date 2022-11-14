7750d89281622d81178054d07b26bbcf8be71f43
Marcel95-sys <farremarcel95@gmail.com>
2022-11-14 20:09:47 +0100

    añadimos delete y put, e instalamos sqlite

--- server.ts
+++ server.ts
@@ -4,12 +4,22 @@ import express from 'express';
 const app = express();
 const PORT:Number=3000;
 
-app.post('/', (req, res) => {
-	
-})
+
 // Handling GET / Request
 app.get('/', (req, res) => {
 	res.send('Welcome to typescript backend!');
+})
+
+app.post('/', (req, res) => {
+	
+})
+
+app.delete('/', (req, res) => {
+	
+})
+
+app.put('/', (req, res) => {
+	
 })
 
 // Server setup

d78be19abf1f515036f19daacd0c947c3849c2ff
Marcel95-sys <farremarcel95@gmail.com>
2022-11-14 21:36:35 +0100

    añadir las instr. referentes a la BD en get y post

--- server.ts
+++ server.ts
@@ -1,20 +1,27 @@
 // Importing module
 import express from 'express';
+import { Database } from 'sqlite3';
 
 const app = express();
 const PORT:Number=3000;
 
+const db = new Database('db.dataBase');
+
 
 // Handling GET / Request
 app.get('/', (req, res) => {
-	res.send('Welcome to typescript backend!');
+	db.get('SELECT ALL() % 100 as result', (_, res) => console.log(res)
+	);	
 })
 
-app.post('/', (req, res) => {
-	
+app.post('/:nick/ :email/ :passw/ :nump', (req, res) => {
+	db.exec('SELECT nick FROM users INSERT ' + req.params.nick);	
+	db.exec('SELECT email FROM users INSERT ' + req.params.email);
+	db.exec('SELECT password FROM users INSERT ' + req.params.passw);
+	db.exec('SELECT numphone FROM users INSERT ' + req.params.nump)
 })
 
-app.delete('/', (req, res) => {
+app.delete('/:id', (req, res) => {
 	
 })
 
